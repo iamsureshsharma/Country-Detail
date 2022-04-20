5a30b2fa88f0af167bfc9e4fe02681388142c86b
iamsureshsharma <suresh1010sharma@gmail.com>
2022-04-20 22:25:34 +0530

    updates base url and adds const

--- lib/utils/const.dart
+++ lib/utils/const.dart
@@ -1,10 +1,9 @@
 // URLs
 
-const String baseUrl = 'https://run.mocky.io/';
-const String basePath = 'v3/c4ab4c1c-9a55-4174-9ed2-cbbe0738eedf';
+const String baseUrl = 'https://run.mocky.io/v3/c4ab4c1c-9a55-4174-9ed2-cbbe0738eedf';
 
 // messages
 const String serverError = "Server is not responding, Please check your connection and try again later!";
 
 //http codes
-const int HTTP_200_OK = 200;
+const int http200ok = 200;
