4a784ed8878d3c962c32f8e628620291e0b96f53
iamsureshsharma <suresh1010sharma@gmail.com>
2022-04-20 08:34:24 +0530

    adds country detail home page

---
+++ lib/screen/country_detail.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CountryDetail extends StatelessWidget {
+  const CountryDetail({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(),
+      ),
+    );
+  }
+}
